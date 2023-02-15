e581982b4e78921fc18dd7e347bd84f5f5bac17a
Volodymyr Hodunko <grordh@gmail.com>
2023-02-14 19:49:44 -0500

    created the game
    fixed bugs with checking for win

---
+++ src/index.js
@@ -0,0 +1,120 @@
+const table = document.getElementsByTagName('td');
+let counter = 0;
+let playerWinCounter = 0;
+let computerWinCounter = 0;
+let tieCounter = 0;
+
+
+/*Add 'X' to game field starts*/
+for (let i = 0; i<table.length; i++) {
+    table[i].addEventListener('click', e => {
+        if(table[i].innerText == '') {
+            table[i].innerText = 'X'
+        }
+
+        setTimeout(compMuve, 250);  //computer move here
+        setTimeout (mainCheck, 501);
+        counter++;
+        setTimeout(tieTimer, 600);
+    })
+}
+/*Add 'X' to game field ends*/
+
+/*Switching from 1 to 2 users block (need to be done) starts*/
+const players = document.getElementById('players');
+
+players.addEventListener('click', e => {
+    if (players.innerText == '1 player') {
+        players.innerText = '2 players'
+        document.getElementById('secondWin').innerText = 'Player 2' + '(' + computerWinCounter + ')'
+    } else {
+        players.innerText = '1 player'
+        document.getElementById('secondWin').innerText = 'Computer' + '(' + computerWinCounter + ')'
+    }
+});
+/*Switching from 1 to 2 users block (need to be done) ends*/
+
+/* window with "press to start statement" starts*/
+const alarmWindow = document.getElementById('alarm');
+
+alarmWindow.addEventListener('click', e =>{
+    alarmWindow.style.display = 'none'
+    for (let i=0; i<table.length; i++) {
+        table[i].innerText = ''
+        counter = 0;
+    }
+})
+/* window with "press to start statement" ends*/
+
+/* Computer moves code starts*/
+function compMuve () {
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+let j = getRndInteger(0, table.length);
+
+    if (table[j].innerText == '') {
+        table[j].innerText = 'O'
+    } else {
+        j = getRndInteger(0, table.length)
+        compMuve()
+    }
+}
+/* Computer moves code ends*/
+
+/* Game logic starts */
+// function tieTimer () {
+//     if ((alarmWindow.innerText != 'Player WON!')&&
+//         (alarmWindow.innerText != 'Computer WON!')
+//         &&(counter == 5)) {
+//         alarmWindow.style.display = 'flex'
+//         alarmWindow.innerHTML = `<h1>` + `TIE!` + `</h1>`;
+//         tieCounter++;
+//         document.getElementById('tie').innerText = 'Tie' + '(' + tieCounter +')';
+//     }
+// }
+
+function tieTimer () {
+    if ((alarmWindow.innerText != 'Player WON!')&&
+        (alarmWindow.innerText != 'Computer WON!')
+        &&(([...table].filter((item) => {
+            return item.innerText == ''
+        })) == false)) {
+        alarmWindow.style.display = 'flex'
+        alarmWindow.innerHTML = `<h1>` + `TIE!` + `</h1>`;
+        tieCounter++;
+        document.getElementById('tie').innerText = 'Tie' + '(' + tieCounter +')';
+    }
+}
+
+
+function checkForWin (a, b, c) {
+    if ((table[a].innerText == table[b].innerText)&&
+        (table[b].innerText == table[c].innerText)&&
+        (table[c].innerText == 'X')) {
+        alarmWindow.style.display = 'flex'
+        alarmWindow.innerHTML = `<h1>` + `Player WON!` + `</h1>`;
+        playerWinCounter++;
+        document.getElementById('firstWin').innerText = 'Player' + '(' + playerWinCounter +')';  
+    } else if ((table[a].innerText == table[b].innerText)&&
+               (table[b].innerText == table[c].innerText)&&
+               (table[c].innerText == 'O')) {
+        alarmWindow.style.display = 'flex'
+        alarmWindow.innerHTML = `<h1>` + `Computer WON!` + `</h1>`;
+        computerWinCounter++; 
+        document.getElementById('secondWin').innerText = 'Computer' + '(' + computerWinCounter + ')';
+    }
+}
+
+function mainCheck () {
+        checkForWin(0, 1, 2),
+        checkForWin(3, 4, 5),
+        checkForWin(6, 7, 8),
+        checkForWin(0, 3, 6),
+        checkForWin(1, 4, 7),
+        checkForWin(2, 5, 8),
+        checkForWin(0, 4, 8),
+        checkForWin(2, 4, 6)
+}
+/* Game logic ends */
